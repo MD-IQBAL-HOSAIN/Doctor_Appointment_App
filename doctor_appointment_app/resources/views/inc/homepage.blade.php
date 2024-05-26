8ea708f7573cb2baaac11155f1deddf492516e33
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-05-27 00:34:42 +0600

    doc. app

--- doctor_appointment_app/resources/views/inc/homepage.blade.php
+++ doctor_appointment_app/resources/views/inc/homepage.blade.php
@@ -9,17 +9,17 @@
 </head>
 <body> --}}
     
-<div>
-    <div id="carouselExampleFade" class="carousel slide carousel-fade">
+<div class="container-fluid">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade mb-2">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="./images/doctor.jpg" class="d-block w-100" style="max-width: 100%; max-height: 600px;" alt="image-1">
+              <img src="./images/operation.jpg" class="d-block w-100" style="max-width: 100%; max-height: 600px; border-radius: 10px;" alt="image-1">
+            </div>
+            <div class="carousel-item">
+              <img src="./images/doctor.jpg" class="d-block w-100" style="max-width: 100%; max-height: 600px; border-radius: 10px;" alt="image-2">
           </div>
           <div class="carousel-item">
-            <img src="./images/operation.jpg" class="d-block w-100" style="max-width: 100%; max-height: 600px;" alt="image-2">
-          </div>
-          <div class="carousel-item">
-            <img src="./images/patient.jpg" class="d-block w-100" style="max-width: 100%; max-height: 600px;" alt="image-3">
+            <img src="./images/patient.jpg" class="d-block w-100" style="max-width: 100%; max-height: 600px; border-radius: 10px;" alt="image-3">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -31,39 +31,43 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+    
+<div>
+    <h1 class="text-center" style="box-shadow: 0 0 10px 0 rgba(22, 102, 102, 0.5); border-radius: 10px">Our Services</h1>
 </div>
 <div>
 <div class="container-fluid p-2 text-center">
   <div class="row">
     <div class="col-12 col-sm-6 col-md-3">
-      <div class="bg-primary p-2">
+      <div class=" p-2">
         <img src="./images/doctor.jpg" class="d-block w-100" style="max-width: 340px; max-height: 250px;" alt="image-1">
         <h5>Neurology</h5>
         <p>Paragraph</p> 
       </div>
     </div>
     <div class="col-12 col-sm-6 col-md-3">
-      <div class="bg-warning p-2">
+      <div class=" p-2">
         <img src="./images/operation.jpg" class="d-block w-100" style="max-width: 340px; max-height: 250px;" alt="image-2">
         <h5>Psychology</h5>
         <p>Paragraph</p> 
       </div>
     </div>
     <div class="col-12 col-sm-6 col-md-3">
-      <div class="bg-danger p-2">
+      <div class="p-2">
         <img src="./images/patient.jpg" class="d-block w-100" style="max-width: 340px; max-height: 250px;" alt="image-3">
         <h5>Dental</h5>
         <p>Paragraph</p> 
       </div>
     </div>
     <div class="col-12 col-sm-6 col-md-3">
-      <div class="bg-success p-2">
+      <div class="p-2">
         <img src="./images/doctor.jpg" class="d-block w-100" style="max-width: 340px; max-height: 250px;" alt="image-1">
         <h5>Orthopedic</h5>
         <p>Paragraph</p> 
       </div>
     </div>
   </div>
+</div>
 </div>
 
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
---
+++ doctor_appointment_app/resources/views/inc/shortnav.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg mt-1" style="background-color: darkcyan; text-color: white; border-radius: 10px; box-shadow: 0 0 10px 0 black;">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="tel:+987007">Call Us : +987007 | </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="tel:+01587876553"><i class="bi bi-telephone"> +8801587876553 | </i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="mailto:author@example.com"><i class="bi bi-envelope"></i> Email: author@example.com</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="{{ URL(' ') }}"><i class="bi bi-arrow-left"></i> Contact US | </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="{{ URL(' ') }}"><i class="bi bi-file-arrow-down"></i> My Appointment</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
