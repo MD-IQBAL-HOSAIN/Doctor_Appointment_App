b907c8f79dc7a5cd6c285038313614e539e4d334
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-06-05 09:54:24 +0600

    doc list

--- doctor_appointment_app/resources/views/doctor/deptdoctors.blade.php
+++ doctor_appointment_app/resources/views/doctor/deptdoctors.blade.php
@@ -1,5 +1,25 @@
+@extends('layouts.main', ['title' => 'Doctor Lists'])
+
+@section('content')   
+
 @forelse ($department->doctors as $doctor)
-    {{ $doctor->name }}
+    {{ $doctor->name }} 
+    - {{ $doctor->email }} 
+    - {{ $doctor->image }} 
+    - {{ $doctor->department_id }} 
+    - {{ $doctor->education }} 
+    - {{ $doctor->experience }} 
+    - {{ $doctor->fees }} 
+    - {{ $doctor->about }} 
+    - {{ $doctor->gender }} 
+    - {{ $doctor->degree }} 
+    - {{ $doctor->university }} 
+    - {{ $doctor->country }} 
+    - {{ $doctor->phone }} 
+    - {{ $doctor->address }}
 @empty
     
 @endforelse
+
+
+@endsection
